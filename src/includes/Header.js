318322bbb7d9d7948eec81a1ec1a0b4bf1754a4d
michelobe <michaeldlr17@gmail.com>
2021-06-27 08:41:32 -0700

    Static header complete.  Styling will need to be tweaked later, there is currently no functionality other than the home link button on the Logo.  Committing changes and pushing to github before I commence work on the body of the main page.

--- src/includes/Header.js
+++ src/includes/Header.js
@@ -14,17 +14,40 @@ export default class Header extends Component {
 
 	render() {
 		return (
-			<div className="container">
-				<header>
-                    <Link to ={`/`} className = {'homeLink'}>
-                        <h1>CityShop</h1>
-                    </Link>
-					<form id="headerSearch">
-						<input type="search" placeholder="Search" />
-						<i className="fas fa-search"></i>
-					</form>
-					<button type="submit">Post An Ad</button>
-				</header>
+			<div>
+				<div className={`topHead`}>
+					<div className={`container`}>
+						<div className={`langLocContainer`}>
+							<div className={`languageSelect`}>
+								<h6>Language:</h6>
+								<h5>English</h5>
+								<i className="fas fa-chevron-down"></i>
+							</div>
+							<div className={`locationSelect`}>
+								<h6>Location:</h6>
+								<h5>Orange</h5>
+								<i className="fas fa-chevron-down"></i>
+							</div>
+						</div>
+						<div className={`accountSelect`}>
+							<i className="fas fa-user"></i>
+							<h6>Account</h6>
+							<i className="fas fa-chevron-down"></i>
+						</div>
+					</div>
+				</div>
+				<div className={`container`}>
+					<div className={`bottomHead`}>
+						<Link to ={`/`} className = {'homeLink'}>
+							<h1 className={`logo`}><span className={`logoLetter`}>C</span>ityShop</h1>
+						</Link>
+						<form id="headerSearch">
+							<input type="search" placeholder="Search" />
+							<i className="fas fa-search"></i>
+						</form>
+						<button type="submit" className={`postBtn`}>Post An Ad</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
