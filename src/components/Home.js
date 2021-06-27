f33d2480c39208807431375f8bcd595ecfad9e04
michelobe <michaeldlr17@gmail.com>
2021-06-27 09:46:43 -0700

    Still styling up the main page, committing changes for now and pushing to github

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,7 @@ export default class CenterCol extends Component {
 	constructor() {
 		super();
 		this.state = {
-            //categoriesData set by componentWillMount,
-            nearbyBool: false
+            //categoriesData set by componentWillMount
 		};
     }
 
@@ -19,20 +18,10 @@ export default class CenterCol extends Component {
         });
     }
 
-    toggleNearby = () => {
-        let nearbyBool = this.state.nearbyBool;
-
-        if(nearbyBool === true || nearbyBool === false){
-            this.setState({
-                nearbyBool: !nearbyBool
-            })
-        }
-    }
-
 	render() {
 		return (
-			<div className={'home'}>
-				<div className="container">
+			<div className={`home`}>
+				<div className={`container`}>
                     <CenterColumn categoriesData = {this.props.categoriesData} />
 				</div>
 			</div>
--- src/components/homeComponents/CenterColumn.js
+++ src/components/homeComponents/CenterColumn.js
@@ -9,15 +9,6 @@ export default class CenterColumn extends Component {
 		super();
 		this.state = {
             //categoriesData set by componentWillMount
-            isActiveOne: false,
-            isActiveTwo: false,
-            isActiveThree: false,
-            isActiveFour: false,
-            isActiveFive: false,
-            isActiveSix: false,
-            isActiveSeven: false,
-            isActiveEight: false,
-            seeMore: false
 		};
     }
     
@@ -118,121 +109,13 @@ export default class CenterColumn extends Component {
     }
     // =============================LOOP LISTING FUNCTIONS===============================
     
-    
-    // =============================TRENDING TAGS FUNCTION===============================
-	loopTags = () => {
-		let testTags = [1, 2, 3, 4, 5, 6, 7];
-
-		return testTags.map(item => {
-			return (
-				<div key={item} className={'tag'}>
-					Apple Macbook
-				</div>
-			);
-		});
-    }
-    // =============================TRENDING TAGS FUNCTION===============================
-    
-    // =============================MEDIA QUERY DROPDOWN===============================
-    changeBoolOne = () => {
-        let isActiveOne = this.state.isActiveOne;
-
-        if(isActiveOne === false){
-            this.setState({
-                isActiveOne: !isActiveOne
-            });
-        }else {
-            this.setState({
-                isActiveOne: !isActiveOne
-            })
-        }
-    }
-    changeBoolTwo = () => {
-        let isActiveTwo = this.state.isActiveTwo;
-
-        if(isActiveTwo === false){
-            this.setState({
-                isActiveTwo: !isActiveTwo
-            });
-        }else {
-            this.setState({
-                isActiveTwo: !isActiveTwo
-            })
-        }
-    }
-    changeBoolThree = () => {
-        let isActiveThree = this.state.isActiveThree;
-
-        if(isActiveThree === false){
-            this.setState({
-                isActiveThree: !isActiveThree
-            });
-        }else {
-            this.setState({
-                isActiveThree: !isActiveThree
-            })
-        }
-    }
-    changeBoolFour = () => {
-        let isActiveFour = this.state.isActiveFour;
-
-        if(isActiveFour === false){
-            this.setState({
-                isActiveFour: !isActiveFour
-            });
-        }else {
-            this.setState({
-                isActiveFour: !isActiveFour
-            })
-        }
-    }
-    changeBoolFive = () => {
-        let isActiveFive = this.state.isActiveFive;
-
-        if(isActiveFive === false){
-            this.setState({
-                isActiveFive: !isActiveFive
-            });
-        }else {
-            this.setState({
-                isActiveFive: !isActiveFive
-            })
-        }
-    }
-    changeBoolSix = () => {
-        let isActiveSix = this.state.isActiveSix;
-
-        if(isActiveSix === false){
-            this.setState({
-                isActiveSix: !isActiveSix
-            });
-        }else {
-            this.setState({
-                isActiveSix: !isActiveSix
-            })
-        }
-    }
-    changeBoolSeven = () => {
-        let isActiveSeven = this.state.isActiveSeven;
-
-        if(isActiveSeven === false){
-            this.setState({
-                isActiveSeven: !isActiveSeven
-            });
-        }else {
-            this.setState({
-                isActiveSeven: !isActiveSeven
-            })
-        }
-    }
-    // =============================MEDIA QUERY DROPDOWN===============================
 
 	render() {
         let categories = this.state.categoriesData;
 
 
 		return (
-            <section id = "centerCol">
+            <section className={`mainPage`}>
                 <div className={'links'}>
                     <div className={'categories'}>
                         <Link to ={`/for-sale`}
@@ -240,10 +123,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[0].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolOne.bind(null)}>
-                            {categories[0].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[0].title === 'jobs' || categories[0].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveOne ? 'active' : ''}`}
                         >
@@ -256,10 +135,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[1].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolTwo.bind(null)}>
-                            {categories[1].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[1].title === 'jobs' || categories[1].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveTwo ? 'active' : ''}`}
                         >
@@ -272,10 +147,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[2].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolThree.bind(null)}>
-                            {categories[2].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[2].title === 'jobs' || categories[2].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveThree ? 'active' : ''}`}
                         >
@@ -288,10 +159,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[3].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolFour.bind(null)}>
-                            {categories[3].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[3].title === 'jobs' || categories[3].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveFour ? 'active' : ''}`}
                         >
@@ -304,10 +171,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[4].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolFive.bind(null)}>
-                            {categories[4].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[4].title === 'jobs' || categories[4].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveFive ? 'active' : ''}`}
                         >
@@ -320,10 +183,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[5].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolSix.bind(null)}>
-                            {categories[5].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[5].title === 'jobs' || categories[5].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveSix ? 'active' : ''}`}
                         >
@@ -336,10 +195,6 @@ export default class CenterColumn extends Component {
                         >
                             {categories[6].title}
                         </Link>
-                        <div className = {'mobileCategory'}
-                            onClick = {this.changeBoolSeven.bind(null)}>
-                            {categories[6].title}
-                        </div>
                         <div
                         className={`groupLinks ${categories[6].title === 'jobs' || categories[6].title === 'housing' ? 'singleCol' : ''} ${this.state.isActiveSeven ? 'active' : ''}`}
                         >
